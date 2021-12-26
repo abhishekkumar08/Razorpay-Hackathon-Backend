b886c7a063ebf60af5b250f66330b7a8f9af06f7
Abhishek Kumar <abhishek022kk@gmail.com>
2021-12-26 19:40:01 +0530

    updated the backend

--- index.js
+++ index.js
@@ -45,11 +45,35 @@ app.post('/verification', (req, res) => {
   res.json({ status: 'ok' });
 });
 
-app.post('/razorpay', async (req, res) => {
+app.post('/razorpay/buy', async (req, res) => {
   const payment_capture = 1;
-  const amount = 499;
+  const amount = 399;
   const currency = 'INR';
+  console.log('req', req);
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture,
+  };
 
+  try {
+    const response = await razorpay.orders.create(options);
+    console.log(response);
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post('/razorpay/rent', async (req, res) => {
+  const payment_capture = 1;
+  const amount = 99;
+  const currency = 'INR';
+  console.log('req', req);
   const options = {
     amount: amount * 100,
     currency,
